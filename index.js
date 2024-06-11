2c5dea27830f198cb0448f96a5348ce03688e00c
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-11 12:02:29 +0530

    Calculator

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 // calculation
 
-const display= document.getElementById("display");
+const display = document.getElementById("display");
 
-
-function appendToDisplay(input){
-display.value +=input;
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-
-function clearDisplay(){
-    display.value= "";
-
+function clearDisplay() {
+  display.value = "";
 }
 
-
-function calculate(){
-    try{
-        display.value=eval(display.value);
-    }
-    catch(error){
-        display.value="Error";
-    }
-
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
